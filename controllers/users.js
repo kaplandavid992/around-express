1271b2926e52e51fcded2ce4c2a088c8dc3de4e1
kaplandavid992 <kaplandavid992@gmail.com>
2022-05-10 12:31:15 +0300

    user id correction

--- controllers/users.js
+++ controllers/users.js
@@ -14,14 +14,14 @@ const getUsers = async (req, res) => {
 const getUserById = async (req, res) => {
   try {
     const users = await getJsonFromFile(usersFilePath);
-    const user = users.find(user => user._id === req.params.id);
+    const user = users.find((user) => user.id === req.params.user_id);
     if (!user) {
       res.status(404).send("User ID not found");
     }
+    res.send(user);
   } catch (error) {
     res.status(500).send("Something went wrong");
   }
-  res.send(user);
 };
 
 module.exports = { getUsers, getUserById };
