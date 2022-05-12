820579a307ddec16983517b26c427f0f438c3e7b
kaplandavid992 <kaplandavid992@gmail.com>
2022-05-12 11:24:22 +0300

    aafter rev. 2

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ const { PORT = 3000 } = process.env;
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 
+const route = (req, res) => {
+  console.log(res.status);
+  res.status(404).send({ message: 'Requested resource not found' });
+};
+
 app.use('/', usersRouter);
 app.use('/', cardsRouter);
+app.get('*', route);
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
 });
